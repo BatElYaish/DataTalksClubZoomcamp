f097915afae7cf710920594289e3ff7a32e67cb4
BatElYaish <batelyaish@gmail.com>
2025-02-21 21:31:48 +0200

    remove time import

--- 1-docker-terraform/Docker-SQL/upload_data.py
+++ 1-docker-terraform/Docker-SQL/upload_data.py
@@ -2,12 +2,11 @@ import pandas as pd
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
-from time import time
-import argparse 
+import argparse
 
 
 def main(params):
-    #get env variables
+    # get env variables
     user = os.getenv('POSTGRES_USER', 'user')
     password = os.getenv('POSTGRES_PASSWORD', 'password')
     host = os.getenv('POSTGRES_HOST', 'localhost')
@@ -15,9 +14,9 @@ def main(params):
     db = os.getenv('POSTGRES_DB', 'db')
 
     table_name = params.table_name
-    url = params.url #get the data file path
+    url = params.url  # get the data file path
 
-    #create sql engine and connection
+    # create sql engine and connection
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     # Open a connection
     try:
@@ -34,57 +33,59 @@ def main(params):
 
     os.system(f"curl -L {url} -o {csv_name}")
 
-    #we only need first rows to get the schema
+    # we only need first rows to get the schema
     df = pd.read_csv(csv_name, nrows=100, low_memory=False)
 
-    #datetime cols array
+    # datetime cols array
     dt_cols = [c for c in df.columns if 'datetime' in c]
 
-    #convert columns to datetime
+    # convert columns to datetime
     for dt_c in dt_cols:
         df[dt_c] = pd.to_datetime(df[dt_c])
 
-    #now we want to only create the table so we'll insert 0 rows of data, if the table exists then drop it and re-create the table
-    df.head(n=0).to_sql(name = table_name, con=engine, if_exists='replace')
+    # now we want to only create the table so we'll insert 0 rows of data,
+    # if the table exists then drop it and re-create the table
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    #if the file is the zones file load the zones else load the taxi files
+    # if the file is the zones file load the zones else load the taxi files
     if 'Zone' in df.columns:
         df.to_sql(name='zones', con=engine, if_exists='replace')
         print(f"All rows loaded to {table_name} table.")
-    else: 
-        #set Chunk size
+    else:
+        # set Chunk size
         chunk_size = 100000
 
-        #read the df using an iterator because the data file is big
+        # read the df using an iterator because the data file is big
         df_iter = pd.read_csv(csv_name, iterator=True, chunksize=chunk_size, low_memory=False)
 
-        #load all iterations
+        # load all iterations
         while True:
             try:
-                #get the next iteration
-                df=next(df_iter)
+                # get the next iteration
+                df = next(df_iter)
 
-                #convert columns to datetime
+                # convert columns to datetime
                 for dt_c in dt_cols:
                     df[dt_c] = pd.to_datetime(df[dt_c])
 
-                #load data
-                df.to_sql(name = table_name, con=engine, if_exists='append')
-                
+                # load data
+                df.to_sql(name=table_name, con=engine, if_exists='append')
+
             except StopIteration:
                 print(f"All rows loaded to {table_name} table.")
                 break
-    
+
     # Check if the file exists
     if os.path.exists(csv_name):
         # Delete the file
         os.remove(csv_name)
 
+
 if __name__ == '__main__':
-    #get arguments from command line
+    # get arguments from command line
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--table_name', help='Table to write results to')
     parser.add_argument('--url', help='url of the csv file')
 
     args = parser.parse_args()
